c7d2dd17893709b80d464e68dedaa0e9a145b86e
Zach Cimafonte <zach@parsely.com>
2012-09-19 03:15:50 -0400

    Added markdown, styling, formatting, publish dates

--- post.py
+++ post.py
@@ -5,6 +5,9 @@ import tornado.httpserver
 from forms import Form
 from wtforms import TextField, TextAreaField
 from wtforms.validators import InputRequired
+from markdown import markdown
+from lib.markdown.mdx_video import VideoExtension
+import datetime as dt
 
 from base import BaseHandler
 from minifier import Minifier
@@ -40,8 +43,7 @@ class PostHandler(BaseHandler):
 
     def index(self):
         # list posts
-        print self.get_current_user().keys()
-        self.vars['posts'] = self.db.posts.find()
+        self.vars.update({'posts': self.db.posts.find()})
         self.render('templates/posts/index.html', **self.vars)
 
     def detail(self, id):
@@ -49,8 +51,7 @@ class PostHandler(BaseHandler):
         res = self.db.posts.find_one({'_id': int(id)})
         if not res:
             raise tornado.web.HTTPError(404)
-
-        self.vars['post'] = res
+        self.vars.update({'post': res})
         self.render('templates/posts/get.html', **self.vars)
 
     # Create a post
@@ -69,11 +70,16 @@ class PostHandler(BaseHandler):
                                                       update={'$inc': {'value': 1}},
                                                       upsert=True, new=True)
         id = counter['value']
+        body = form.body.data
+        video_ext = VideoExtension(configs={})
+        body_html = markdown(body, extensions=[video_ext], output_format='html5', safe_mode=False)
         post = {
             'title': form.title.data,
-            'body': form.body.data,
+            'body_html': body_html,
+            'body_raw': body,
             '_id': id,
             'user': self.get_current_user(),
+            'date_created': dt.datetime.now(),
         }
         self.db.posts.insert(post)
         self.redirect('/posts/%s' % minifier.int_to_base62(post['_id']))
