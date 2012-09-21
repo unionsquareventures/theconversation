9172e1d69112155d4b288c8b945343dedabd3ea5
Zach Cimafonte <zach@parsely.com>
2012-09-21 13:15:48 -0400

    Added base model class

--- models/post.py
+++ models/post.py
@@ -4,14 +4,10 @@ from lib.markdown.mdx_video import VideoExtension
 import datetime as dt
 
 from mongoengine import *
+from base import BaseDocument
 from user import User
 
-from minifier import Minifier
-
-minifier = Minifier()
-
-class Post(Document):
-    id = IntField(primary_key=True)
+class Post(BaseDocument):
     title = StringField(required=True, max_length=1000)
     body_raw = StringField(required=True)
     body_html = StringField(required=True)
@@ -23,40 +19,5 @@ class Post(Document):
     }
     ignored_fields = ['body_html']
 
-    def minified_id(self):
-        return minifier.int_to_base62(self.id)
-
-    def hook_id(self):
-        counter = self._get_db().postsCounter.find_and_modify(query={'_id': 'object_counter'},
-                                                      update={'$inc': {'value': 1}},
-                                                      upsert=True, new=True)
-        id = counter['value']
-        self._data['id'] = id
-
     def hook_date_created(self):
         self._data['date_created'] = dt.datetime.now()
-
-    def save(self, *args, **kwargs):
-        if kwargs.get('force_insert') or '_id' not in self.to_mongo():
-            self.hook_id()
-            self.hook_date_created()
-
-        super(Post, self).save(*args, **kwargs)
-
-    def form_fields(self, form_errors=None):
-        for name, field in self._fields.iteritems():
-            if name == self._meta['id_field'] or name in self.ignored_fields:
-                continue
-
-            if field.__class__ != StringField:
-                continue
-            field_html = '<textarea name="{name}">{value}</textarea>'
-            if field.__class__ == StringField and field.max_length:
-                field_html = '<input name="{name}" type="text" value="{value}" />'
-
-            value = self._data.get(name)
-            value = value.replace('"', '\\"') if value else ''
-            field_html = field_html.format(name=name, value=value)
-            label = self.labels.get(name, name).title()
-            field_errors = form_errors.get(name)
-            yield (name, label, field_html, field_errors)
