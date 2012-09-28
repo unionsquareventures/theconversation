fb6f15b75abec3679c48598b95fa3cc6bc8d277b
Zach Cimafonte <zach@parsely.com>
2012-09-28 12:19:49 -0400

    Added disqus box for annotations.

--- handlers/disqus.py
+++ handlers/disqus.py
@@ -12,11 +12,17 @@ class DisqusHandler(BaseHandler):
         post = Post.objects(id=self.get_argument('post_id')).first()
         if not post:
             raise tornado.web.HTTPError(404)
-        question_number = int(self.get_argument('question_number'))
+
+        try:
+            q_number = self.get_argument('question_number')
+            secondary_id = 'question' + str(int(q_number))
+        except:
+            a_number = self.get_argument('annotation_number')
+            secondary_id = 'annotation' + str(int(a_number))
 
         self.vars.update({
             'post': post,
-            'question_number': question_number,
+            'secondary_id': secondary_id,
         })
         self.render('disqus/index.html', **self.vars)
 
