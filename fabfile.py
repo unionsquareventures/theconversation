f547aa18397875c1d58bf1135c31bc56ca2cc065
Zach Cimafonte <zach@parsely.com>
2012-10-05 11:17:22 -0400

    Added fabfile for deployment.

---
+++ fabfile.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+from fabric.api import *
+
+env.user = 'ubuntu'
+env.hosts = [
+    'ec2-184-72-183-210.compute-1.amazonaws.com'
+]
+
+def deploy():
+    deploy_path = '/data/apps/usv'
+    run('sudo mkdir -p %s' % deploy_path)
+    run('sudo chmod 777 %s' % deploy_path)
+    local('find . -name "*.pyc" -exec rm -rf {} \;')
+    local('scp -r -i ../usv.pem *'
+                        ' ubuntu@%s:%s' % (env.hosts[0], deploy_path))
+    with cd(deploy_path):
+        clean = lambda x: x.replace('/', '\\\\/').replace('"', '\\\\"')
+
+        # Switch settings to production
+        initial = 'DEPLOYMENT_STAGE = "local"'
+        new = 'DEPLOYMENT_STAGE = "production"'
+        run('sed -i "s/%s/%s/g" %s' % (clean(initial), clean(new), "settings.py"))
+
+        # Switch Prod config
+        initial = "server_address = 'insert_address_here'"
+        new = "server_address = 'https://ec2-107-22-140-36.compute-1.amazonaws.com'"
+        run('sed -i "s/%s/%s/g" %s' % (clean(initial),
+                                        clean(new),
+                                        "server_setup/deployment/prod.py"))
+
+        run('sudo mkdir -p ../usv_ssl')
+        run('sudo chmod 777 ../usv_ssl')
+        run('cp -r ../usv/* ../usv_ssl')
+        with cd('/data/apps/usv_ssl'):
+            # Switch Prod port for SSL
+            initial = "server_port = 80"
+            new = "server_port = 443"
+            run('sed -i "s/%s/%s/g" %s' % (clean(initial),
+                                            clean(new),
+                                            "server_setup/deployment/prod.py"))
+
+        # Restart server processes
+        run('sudo supervisorctl restart usv_group:*')
