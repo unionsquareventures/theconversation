1195876860d866d707c35164ed908123bc95b361
Zach Cimafonte <zach@parsely.com>
2012-10-24 21:58:45 -0400

    Adding links

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 USV web presence development.
 
 When running locally be sure to use http://127.0.0.1:8888/ (not localhost or 0.0.0.0, etc.) Twitter OAuth is setup to use this address.
+
+Phantom JS must be in /usr/bin/phantomjs for thumbnails
---
+++ scripts/rasterize.js
@@ -0,0 +1,28 @@
+var page = require('webpage').create(),
+    system = require('system'),
+    address, output, size;
+
+if (system.args.length < 3 || system.args.length > 5) {
+    console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
+    console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
+    phantom.exit(1);
+} else {
+    address = system.args[1];
+    output = system.args[2];
+    page.viewportSize = { width: 600, height: 600 };
+    if (system.args.length > 4) {
+        page.zoomFactor = system.args[4];
+    }
+
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+        } else {
+            window.setTimeout(function () {
+                page.clipRect = { top: 0, left: 0, width: 800, height: 600 };
+                page.render(output);
+                phantom.exit();
+            }, 200);
+        }
+    });
+}
