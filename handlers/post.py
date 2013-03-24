3e1c40376cd4af33d1003f238ba8c1cec8c7a760
Zach Cimafonte <zach@parsely.com>
2013-03-24 15:34:11 -0400

    Fixup truncation

--- handlers/post.py
+++ handlers/post.py
@@ -38,7 +38,10 @@ class PostHandler(BaseHandler):
         featured_posts = list(Content.objects(featured=True).order_by('-date_created'))
 
         for post in featured_posts:
-                post['body_html'] = truncate(post['body_html'], 500, ellipsis='')
+                try:
+                    post['body_html'] = truncate(post['body_html'], 500, ellipsis='')
+                except:
+                    pass
                 post['body_html'] = html_sanitize_preview(post['body_html'])
         tags = Tag.objects()
         self.vars.update({
