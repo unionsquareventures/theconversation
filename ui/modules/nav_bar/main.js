e4a14990709fbbba237a563ee50c7a172c317a88
Zach Cimafonte <zach@parsely.com>
2013-06-04 19:49:18 -0400

    Fixup navigation buttons on mobile.

--- ui/modules/nav_bar/main.js
+++ ui/modules/nav_bar/main.js
@@ -1,7 +1,4 @@
 $(function () {
-    $('#navigation_bar .submit_content').on("click", function(e) {
-        window.location = "/links/new";
-    });
     $("#navigation_button").tappable(function() {
       $("#navigation_button").toggleClass("active");
       $("#navigation_items").collapse('toggle');
