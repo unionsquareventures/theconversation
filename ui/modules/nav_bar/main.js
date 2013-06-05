8ac4550ebbebbd76830114753ba56b26ba1e5549
Zach Cimafonte <zach@parsely.com>
2013-06-05 16:42:42 -0400

    Fix clicking on mobile, fix the avatars of mobile featured posts on the homepage.

--- ui/modules/nav_bar/main.js
+++ ui/modules/nav_bar/main.js
@@ -1,5 +1,10 @@
 $(function () {
-    $("#navigation_button").tappable(function() {
+    $("#account_settings_btn").click(function(e) {
+      $("#user_modal").modal('show');
+      e.stopPropagation();
+    });
+
+    $("#navigation_button").click(function(e) {
       $("#navigation_button").toggleClass("active");
       $("#navigation_items").collapse('toggle');
     });
