f0d3ddd00b80ec4acb25922a63a609b1b921d3f2
Zach Cimafonte <zach@parsely.com>
2013-07-27 21:30:41 -0400

    Renamed Deleted Content -> Deleted Posts

---
+++ handlers/deleted_posts.py
@@ -0,0 +1,16 @@
+from base import BaseHandler
+from models import Post
+import tornado.web
+
+class DeletedPostsHandler(BaseHandler):
+    def get(self):
+        if not self.is_admin():
+            raise tornado.web.HTTPError(403)
+
+        cur_page = 0
+        length = 50
+        deleted_posts = Post.objects(deleted=True).order_by('-date_created')[cur_page*length:cur_page+length]
+        self.vars.update({
+            'deleted_posts': deleted_posts,
+        })
+        self.render('deleted_posts/index.html', **self.vars)
