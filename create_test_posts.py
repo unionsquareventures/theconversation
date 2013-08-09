7b48323b435887321f5420e9e5ccec53c5ad36eb
Sumit Paul <paul.sumit@gmail.com>
2013-08-09 16:17:55 -0400

    Fixed the creation script.

--- create_test_posts.py
+++ create_test_posts.py
@@ -1,8 +1,16 @@
 import models
 import redis
 import datetime as dt
-from lib.score import calculate_score
 import time
+from math import log
+
+def calculate_score(votes, date_created):
+    adjusted_votes = log(max(abs(votes), 1), 10)
+    sign = 1
+    age_factor = 45000.0 # ~12.5 hour increments
+    timestamp = time.mktime(date_created.timetuple())
+    score = adjusted_votes + round(sign * timestamp / age_factor)
+    return score
 
 user = {
    'auth_type': 'twitter',
