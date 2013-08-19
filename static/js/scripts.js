aeb24941a24f12372ed246ab1ff52b0b88dd067a
Sumit Paul <paul.sumit@gmail.com>
2013-08-19 17:46:17 -0400

    added share buttons, adjusted profile thumbnail for tablets

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -109,4 +109,42 @@ $(function() {
     $(this).toggleClass('checked');
   });
 
+  //share widget
+  $('.share-twitter').sharrre({
+    share: {
+      twitter: true
+    },
+    enableHover: false,
+    enableTracking: true,
+    click: function(api, options){
+      api.simulateClick();
+      api.openPopup('twitter');
+    }
+  });
+
+  $('.share-facebook').sharrre({
+    share: {
+      facebook: true
+    },
+    enableHover: false,
+    enableTracking: true,
+    click: function(api, options){
+      api.simulateClick();
+      api.openPopup('facebook');
+    }
+  });
+
+  $('.share-googleplus').sharrre({
+    share: {
+      googlePlus: true
+    },
+    enableHover: false,
+    enableTracking: true,
+    urlCurl: '',
+    click: function(api, options){
+      api.simulateClick();
+      api.openPopup('googlePlus');
+    }
+  });
+
 });
