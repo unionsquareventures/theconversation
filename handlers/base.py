644c9b772a3486990bde22ab1a60664d39881008
Zach Cimafonte <zachcimafonte@gmail.com>
2013-09-09 02:49:58 -0400

    .

--- handlers/base.py
+++ handlers/base.py
@@ -11,7 +11,7 @@ class BaseHandler(SentryMixin, tornado.web.RequestHandler):
         super(BaseHandler, self).__init__(*args, **kwargs)
         self.vars = {
                         'user_id_str': self.get_current_user_id_str(),
-                        'user_email_address': self.get_secure_cookie('email_address'),
+                        'user_email_address': self.get_secure_cookie('email_address') or '',
                         'settings': settings,
                         'is_admin': self.is_admin,
                         'urlparse': urlparse,
