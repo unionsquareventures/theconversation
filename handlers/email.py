55fd8581bd97d6bf69e69dea2c134a497d515fe0
Zach Cimafonte <zachcimafonte@gmail.com>
2013-09-19 01:49:06 -0400

    Remove email validation.

--- handlers/email.py
+++ handlers/email.py
@@ -22,7 +22,6 @@ class EmailHandler(BaseHandler):
 
         # Subscribe a user to the thread specified in response
         def _subscribe(response):
-            print "== SUBSCRIBE"
             if not response:
                 return
             thread_id = response['id']
@@ -31,9 +30,7 @@ class EmailHandler(BaseHandler):
         # Subscribe the user to thread if it was created successfully.
         # Otherwise retrieve the thread ID and the subscribe them.
         def _created(response):
-            print "== CREATED"
             if not response:
-                print "== DETAILS"
                 disqus.thread_details(_subscribe, post.id)
             else:
                 _subscribe(response)
@@ -47,34 +44,12 @@ class EmailHandler(BaseHandler):
 
     @tornado.web.authenticated
     def get(self):
-        token = self.get_argument('token', '')
-        if token:
-            ev = EmailVerification.objects.get(token=token)
-            id_str = self.get_current_user_id_str()
-            if id_str != ev.user_id_str:
-                raise tornado.web.HTTPError(500)
-            u = UserInfo.objects.get(user__id_str=id_str)
-            u.email_address = ev.email_address
-            u.save()
-            ev.delete()
-            self.set_secure_cookie('email_address', ev.email_address)
-            if ev.subscribe_to:
-                p = Post.objects.get(id=ev.subscribe_to)
-                self.subscribe(u, p)
-            self.vars.update({
-                'email': ev.email_address,
-                'next': ev.next or '/',
-                'errors': '',
-                'status': 'email_verified',
-            })
-            self.render('email/index.html', **self.vars)
-            return
         next = self.get_argument('next', '')
         subscribe_to = self.get_argument('subscribe_to', '')
         u = UserInfo.objects.get(user__id_str=self.get_current_user_id_str())
         self.vars.update({
             'email': u.email_address or '',
-            'errors': '',
+            'error': '',
             'next': next,
             'subscribe_to': subscribe_to,
             'status': 'enter_email',
@@ -89,46 +64,37 @@ class EmailHandler(BaseHandler):
         self.subscribe_to = self.get_argument('subscribe_to', '')
         self.token = str(uuid.uuid4())
         self.link = 'http://%s/auth/email/?token=%s' % (settings.base_url, self.token)
+        id_str = self.get_current_user_id_str()
+        u = UserInfo.objects.get(user__id_str=id_str)
         # Clear the existing email address
         if not self.email:
-            id_str = self.get_current_user_id_str()
-            u = UserInfo.objects.get(user__id_str=id_str)
             u.email_address = ''
+            self.set_secure_cookie('email_address', '')
             u.save()
             self.vars.update({
                 'email': '',
-                'errors': '',
+                'error': '',
                 'next': self.next,
                 'subscribe_to': self.subscribe_to,
                 'status': 'enter_email_cleared',
             })
             self.render('email/index.html', **self.vars)
             return
-        sendgrid = self.settings['sendgrid']
-        sendgrid.send_email(self._on_sendgrid, **{
-            'from': 'no_reply@usv.com',
-            'to': self.email,
-            'subject': "USV.com - Please verify your email address",
-            'text': "Please verify your email address by navigating to this link: %s" % self.link,
-        })
-
-    @tornado.web.asynchronous
-    def _on_sendgrid(self, success):
-        if not success:
-            raise tornado.web.HTTPError(500)
-
-        v = EmailVerification(**{
-            'email_address': self.email,
-            'user_id_str': self.get_secure_cookie('user_id_str'),
-            'next': self.next,
-            'subscribe_to': self.subscribe_to,
-            'token': self.token,
-        })
-        v.save()
-        self.vars.update({
-            'email': self.email,
-            'errors': '',
-            'next': self.next,
-            'status': 'verification_email',
-        })
-        self.render('email/index.html', **self.vars)
+        existing = UserInfo.objects(email_address=self.email).first()
+        if existing and existing.user.id_str != id_str:
+            self.vars.update({
+                'email': self.email or '',
+                'error': 'This email address is already in use.',
+                'next': self.next,
+                'subscribe_to': self.subscribe_to,
+                'status': 'enter_email',
+            })
+            self.render('email/index.html', **self.vars)
+            return
+        u.email_address = self.email
+        u.save()
+        self.set_secure_cookie('email_address', self.email)
+        if self.subscribe_to:
+            p = Post.objects.get(id=self.subscribe_to)
+            self.subscribe(u, p)
+        self.redirect(self.next or '/')
