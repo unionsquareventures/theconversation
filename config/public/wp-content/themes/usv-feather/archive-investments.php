d68b3cdf0885eecd92e2568ff25f2efc5728c694
Nick Grossman <nick@wrkng.net>
2013-09-22 08:31:19 -0400

    fix for logos padding

--- config/public/wp-content/themes/usv-feather/archive-investments.php
+++ config/public/wp-content/themes/usv-feather/archive-investments.php
@@ -46,7 +46,7 @@
 
 				<div class="col-sm-4 col-xs-6 company-container current" usv:investment_series="<?php the_field('investment_series'); ?>" usv:city="<?php the_field('city'); ?>" usv:investment_date="<?php the_field('investment_date'); ?>" usv:investment_categories="<?php echo($categories); ?>">
 					<div class="company">
-					<a href="<?php the_permalink(); ?>"><img alt="<?php the_title(); ?>" src="<?php the_field('logo'); ?>"  class="logo mt-image-none <?php if (get_field('white_logo')) : ?>white<?php endif; ?>" style="" width="220" height="150"></a>
+					<a href="<?php the_permalink(); ?>"><img alt="<?php the_title(); ?>" src="<?php the_field('logo'); ?>"  class="logo mt-image-none<?php if (get_field('extra_logo_padding')) : ?> needs-padding<?php endif; ?>" style="" width="220" height="150"></a>
 					<p class="summary">
 						<?php 
 							if( $post->post_excerpt ) {
@@ -94,9 +94,8 @@
 				<div class="col-sm-4 col-xs-6 company-container past" usv:investment_series="<?php the_field('investment_series'); ?>" usv:city="<?php the_field('city'); ?>" usv:investment_date="<?php the_field('investment_date'); ?>" usv:investment_categories="<?php echo($categories); ?>">
 					<div class="company">
 					<a href="<?php the_permalink(); ?>">
-						<img alt="<?php the_title(); ?>" src="<?php the_field('logo'); ?>"  class="logo mt-image-none<?php if (get_field('extra_logo_padding')) : ?>needs-padding<?php endif; ?>" width="220" height="150"></a>
+						<img alt="<?php the_title(); ?>" src="<?php the_field('logo'); ?>"  class="logo mt-image-none<?php if (get_field('extra_logo_padding')) : ?> needs-padding<?php endif; ?>" width="220" height="150"></a>
 					<p class="summary">
-						<?php the_field('extra_logo_padding'); ?>
 						<?php 
 							if( $post->post_excerpt ) {
 								echo '<a href="'. get_permalink() . '">' . get_the_title() .'</a> ';
