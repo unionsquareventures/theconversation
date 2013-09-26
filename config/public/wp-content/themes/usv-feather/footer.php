5f2b17d042be8fb09a8670dc7b2ccace053bae2f
Nick Grossman <nick@wrkng.net>
2013-09-26 12:39:05 -0400

    whoops - I put replace in the wrong place and didn't test.  fixed now

--- config/public/wp-content/themes/usv-feather/footer.php
+++ config/public/wp-content/themes/usv-feather/footer.php
@@ -90,7 +90,7 @@
                         if ($.inArray(cities[i], city_options) == -1) {
                             // we haven't seen this one yet
                             if (cities[i] != "" && typeof(cities[i]) != "undefined") {
-                                city_options.push(cities[i]).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+                                city_options.push(cities[i].replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
                             }
                         }
                     }
@@ -120,7 +120,7 @@
                         if ($.inArray(categories[i], category_options) == -1) {
                             // we haven't seen this one yet
                             if (categories[i] != "" && typeof(categories[i]) != "undefined") {
-                                category_options.push(categories[i]).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+                                category_options.push(categories[i].replace(/^\s\s*/, '').replace(/\s\s*$/, ''));
                             }
                         }
                     }
