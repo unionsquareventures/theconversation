f4ae92dcf35cc5bd46a69bd83a2e8fbe6a1baab0
Nick Grossman <nick@wrkng.net>
2013-10-01 11:58:56 -0400

    add updates to network page

--- config/public/wp-content/themes/usv-feather/template-network.php
+++ config/public/wp-content/themes/usv-feather/template-network.php
@@ -23,12 +23,26 @@ Template Name: Network
             <div class="row">
                 <div class="col-sm-6">
                     <?php the_field('network_page_content'); ?>
-                </div>
-                <div id="slideshow" class="col-sm-6" style="text-align:right; padding-top:20px">
-                    <iframe align="center" src="http://www.flickr.com/slideShow/index.gne?group_id=&user_id=83782618@N00&set_id=72157629963182717&text=" frameBorder="0" width="450" height="450" scrolling="no"></iframe> 
-                </div>
+                </div><!-- /.col-sm-6 -->
+                <div id="slideshow" class="col-sm-6" style="padding-top:20px">
+                    <iframe align="center" src="http://www.flickr.com/slideShow/index.gne?group_id=&user_id=83782618@N00&set_id=72157629963182717&text=" frameBorder="0" width="500" height="500" scrolling="no"></iframe> <br /><br />
+                    
+                    
                 
-            </div>
+                </div><!-- /col-sm-6 -->
+                
+            </div><!-- /.row -->
+            <h2 class="section-heading">Network Updates</h2>
+            <div class="row">
+                <?php for ($i = 0; $i < 6; $i++) : ?>
+                    <div class="col-sm-4">
+                        <div class="post" style="background: #eee; border-bottom: 1px solid #ddd; margin-bottom: 40px; padding: 20px;">
+                            <h3>This is a post</h3>
+                            <p>With some stuff</p>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+                </div><!-- /row -->
                             
         </article>
     </div><!--end of main-->
