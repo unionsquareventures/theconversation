8439dde32cafbb1e2940b6627fe1aedd4ab55420
AlexanderPease <alexanderpease@usv.com>
2013-10-03 11:24:27 -0400

    removed zander's venv from repo

--- venv/lib/python2.7/site-packages/newrelic-2.0.0.1/newrelic/api/settings.py
+++
@@ -1,7 +0,0 @@
-import newrelic.core.config
-
-settings = newrelic.core.config.global_settings
-
-RECORDSQL_OFF = 'off'
-RECORDSQL_RAW = 'raw'
-RECORDSQL_OBFUSCATED = 'obfuscated'
--- venv/lib/python2.7/site-packages/newrelic-2.0.0.1/newrelic/hooks/external_dropbox.py
+++
@@ -1,9 +0,0 @@
-import newrelic.api.external_trace
-
-def instrument(module):
-
-    def url_request(rest_obj, method, url, *args, **kwargs):
-        return url
-
-    newrelic.api.external_trace.wrap_external_trace(
-            module, 'rest.RESTClientObject.request', 'dropbox', url_request)
