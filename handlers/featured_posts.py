8f2ed2cce214d3bb466e7ca50f5d7d63d479dc28
Nick Grossman <nick@wrkng.net>
2013-10-10 17:58:45 -0400

    show 9 featured posts at once in the "more" page

--- handlers/featured_posts.py
+++ handlers/featured_posts.py
@@ -5,7 +5,7 @@ import tornado.web
 class FeaturedPostsHandler(BaseHandler):
     def get(self):
         page = abs(int(self.get_argument('page', '1')))
-        per_page = abs(int(self.get_argument('per_page', '10')))
+        per_page = abs(int(self.get_argument('per_page', '9')))
         featured_posts = Post.objects(deleted=False, featured=True).order_by('-date_featured')
         featured_posts = featured_posts[(page-1)*per_page:(page-1)*per_page+per_page]
         total_count = Post.objects(deleted=False, featured=True)\
