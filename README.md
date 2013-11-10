2924e92c1ae9965036770e3798fe9b15bf0e009e
Kevin Marshall <info@falicon.com>
2013-11-10 09:50:43 -0500

    core organization updates

---
+++ README.md
@@ -0,0 +1,22 @@
+USV
+======
+
+USV.com community site.
+
+Built with:
+
+ * Python / [Tornado](http://tornadoweb.org)
+ * Mongodb and [Mongoengine](http://mongoengine.org)
+ * [Redis](http://redis.io)
+ * [Disqus](http://disqus.com)
+ * [Twitter](http://dev.twitter.com)
+ 
+Installation
+------------
+
+ * Start the web server:
+
+ python tornado_server.py
+
+ * Site should be viewable at http://localhost:8001
+
--- lib/mongo.py
+++ lib/mongo.py
@@ -2,6 +2,7 @@ import logging
 import pymongo
 import settings
 
+
 class Proxy(object):
   _db = None
   def __getattr__(self, name):
@@ -9,8 +10,7 @@ class Proxy(object):
       # lazily connect to the db so we pickup the right environment settings
       mongo_database = settings.get('mongo_database')
       logging.info("connecting to mongo at %s:%d/%s" % (mongo_database['host'], mongo_database['port'], mongo_database['db']))
-      slave_ok = settings.get('read_only') and True or False
-      connection = pymongo.Connection(mongo_database['host'], mongo_database['port'], slave_okay=slave_ok, network_timeout=2)
+      connection = pymongo.MongoClient(mongo_database['host'], mongo_database['port'], connectTimeoutMS=5000, max_pool_size=200)
       Proxy._db = connection[mongo_database['db']]
 
     return getattr(self._db, name)
