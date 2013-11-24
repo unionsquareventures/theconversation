4010dd3737984b88b5156a81cdeb5f5adc28085b
Kevin Marshall <info@falicon.com>
2013-11-24 14:44:23 -0500

    profile updates, basic stats features

--- lib/tagsdb.py
+++ lib/tagsdb.py
@@ -23,5 +23,14 @@ def get_hot_tags():
   ])
   return tags
 
+def get_user_tags(screen_name):
+  tags = db.post.aggregate([
+    {'$unwind':'$tags'},
+    {'$match': {'user.screen_name':screen_name}},
+    {'$group': {'_id': '$tags', 'count': {'$sum': 1}}},
+    {"$sort": SON([("count", -1), ("_id", -1)])}
+  ])
+  return tags
+
 def save_tag(tag):
   return db.tag.update({'name':tag}, {'name':tag}, upsert=True)
