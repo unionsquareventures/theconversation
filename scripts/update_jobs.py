304bd8b5e0ba416c70999a43e2a50502e6b67b16
AlexanderPease <alexander@usv.com>
2013-12-11 18:44:14 -0500

    commented out local settings import in update_jobs.py

--- scripts/update_jobs.py
+++ scripts/update_jobs.py
@@ -1,8 +1,8 @@
 # Run by Heroku scheduler every night
 # If running locally, uncomment below imports
-import sys
-sys.path.insert(0, '/Users/AlexanderPease/git/usv/website/usv')
-import settings
+#import sys
+#sys.path.insert(0, '/Users/AlexanderPease/git/usv/website/usv')
+#import settings
 
 from lib import jobsdb
 
