241ce1998f03984cd3c7ede61a226c38b66e1838
AlexanderPease <alexander@usv.com>
2013-12-17 13:01:39 -0500

    Let any can_edit() users edit posts. Previously only the author actually had their edits saved. Also starting to fix ironworker

--- scripts/update_jobs.py
+++ scripts/update_jobs.py
@@ -1,7 +1,7 @@
 # Run by Heroku scheduler every night
 # If running locally, uncomment below imports
-import sys
-sys.path.insert(0, '/Users/AlexanderPease/git/usv/website/usv')
+#import sys
+#sys.path.insert(0, '/Users/AlexanderPease/git/usv/website/usv')
 import settings
 
 from lib import jobsdb
