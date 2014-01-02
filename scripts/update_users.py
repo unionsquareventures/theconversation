71564140e7c6095b8c24fe7b2b0c4d45e3857c4a
Nick Grossman <nick@wrkng.net>
2014-01-02 16:19:46 -0500

    add posts to twitter avatar update script

--- scripts/update_users.py
+++ scripts/update_users.py
@@ -1,7 +1,7 @@
 # Run by Heroku scheduler every night
 # If running locally, uncomment below imports
 import sys
-sys.path.insert(0, '/Users/AlexanderPease/git/usv/website/usv')
+sys.path.insert(0, '/Users/nick/dev/usv/usv.com')
 import settings
 
 from lib import userdb
